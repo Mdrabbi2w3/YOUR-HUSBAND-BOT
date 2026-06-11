969fe6a3099a31d1b7c325f442e2d39e21c2dfe9
FR RABBI <mdrabbihossen3125@gmail.com>
2026-06-12 00:13:04 +0600

    Add initial settings configuration for bot

---
+++ settings.js
@@ -0,0 +1,17 @@
+const settings = {
+  packname: 'YOUR HUSBAND ',
+  author: 'RABBI',
+  botName: "YOUR HUSBAND BOT",
+  botOwner: 'RABBI',
+  botOwnerNumber: '8801761554035', // Bot Owner Name er niche number property add kora hoyeche
+  ownerNumber: '8801761554035', 
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.7",
+  updateZipUrl: "https://github.com/Mdrabbi2w3/YOUR-HUSBAND-BOT.git",
+};
+
+module.exports = settings;
