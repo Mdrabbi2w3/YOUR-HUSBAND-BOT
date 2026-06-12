01481b8d2da297281a11b22b34ddcb2e65957f80
FR RABBI <mdrabbihossen3125@gmail.com>
2026-06-12 19:49:25 +0600

    Frist commit

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
- * YOUR HUSBAND Bot - A WhatsApp Bot
- * Copyright (c) 2026 YOUR HUSBAND
- * * This program is free software: you can redistribute it and/or modify
- * it under the terms of the MIT License.
- */
+// * YOUR HUSBAND Bot - A WhatsApp Bot
+// * Copyright (c) 2026 YOUR HUSBAND
+// * * This program is free software: you can redistribute it and/or modify
+// * it under the terms of the MIT License.
 require('!/settings')
 const { Boom } = require('@hapi/boom')
 const fs = require('fs')
