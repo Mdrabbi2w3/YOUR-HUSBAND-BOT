00fd6ad89a3d8b92ffac90e289a90334c7db0210
FR RABBI <mdrabbihossen3125@gmail.com>
2026-06-12 17:30:49 +0600

    Frist commit

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-// * YOUR HUSBAND BOT - Main Configuration Profile Settings
+// YOUR HUSBAND BOT - Main Configuration Profile Settings
  * Copyright (c) 2026 Professor
  * * This program is free software: you can redistribute it and/or modify
  * it under the terms of the MIT License.
